a7207f8f3ba899755d28dd5e7be6404c515fbab1
Olajide Samson Yomi <olajideyormith96@gmail.com>
2022-09-12 09:04:26 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ *main - prints alphabets in lowercase
+ *Return:0 success
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}

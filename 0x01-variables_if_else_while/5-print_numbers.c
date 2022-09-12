012ef03a7b1ad116ff224d85c77dd49607b3e30b
Olajide Samson Yomi <olajideyormith96@gmail.com>
2022-09-12 09:53:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - printing numbers
+ *Return:0 success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n);
+
+	return (0);
+}
+

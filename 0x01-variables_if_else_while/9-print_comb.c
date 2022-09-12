ecd02287d218652661aa37663ab88480daac01f1
Olajide Samson Yomi <olajideyormith96@gmail.com>
2022-09-12 11:42:27 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - printsnumbers separated by commas
+ *Return:0 success
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}

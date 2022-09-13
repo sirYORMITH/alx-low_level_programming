89d3e4fd4ff7f7bdc8f9eea2170c7afc556ec7c5
Olajide Samson Yomi <olajideyormith96@gmail.com>
2022-09-13 12:19:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet - function to prints alphabet in lowercase
+ *Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}

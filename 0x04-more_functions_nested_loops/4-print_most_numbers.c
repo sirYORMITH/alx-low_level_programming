b0068861334c29715af47e5e1969c8631092d2c6
Olajide Samson Yomi <olajideyormith96@gmail.com>
2022-09-15 13:31:35 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*function_name: print_most_numbers - prints numbers
+*Description: prints from 0 - 9 except 2 and 4
+*/
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9' c++)
+	{
+		if (c == '2' || c == '4')
+			continue;
+	_putchar(c);
+	}
+
+	_putchar('\n');
+}

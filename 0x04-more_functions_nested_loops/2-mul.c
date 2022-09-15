778263bb45e6e7ac6be1597c106e8c9629bbfbbb
Olajide Samson Yomi <olajideyormith96@gmail.com>
2022-09-15 13:08:55 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*function_name: mul - multiples 2 integers
+*Description: rturns the product of two numbers
+*@a: first input
+*@b: second input
+*Return: a * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}

e51863f26668bc1df58afc9a922be5c3a5e4792d
Olajide Samson Yomi <olajideyormith96@gmail.com>
2022-09-20 12:47:18 -0700

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *reset_to_98 - updates poiinter:
+ *Description: updates the value it points to to 98.
+ *@n: input pointer
+ *Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

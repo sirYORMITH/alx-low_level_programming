75ec1b56e2ccaf3ea17b3e0e7706a1967901597d
Olajide Samson Yomi <olajideyormith96@gmail.com>
2022-09-20 12:59:53 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_puts - writes output to stdout
+ *Description: - writes str to the stdout
+ *@str: What gets written
+ *Return: void
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
